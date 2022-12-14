940d59fac7b8350c849e64b4696a302a6145c62f
Marcin Wrembel <marcin.wrembel@interia.pl>
2022-12-14 11:50:12 +0100

    up

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ const countryContainer = document.querySelector('div.country-info');
 
 function showCountryFlagName(data) {
   const newListElement = document.createElement('li');
-  newListElement.classList.add('js-country-list__item');
+  newListElement.classList.add('country-list__item');
 
   const countryDescription = document.createElement('span');
-  countryDescription.classList.add('js-country-list__name');
+  countryDescription.classList.add('country-list__name');
   countryDescription.textContent = data.name.official;
 
   const countryFlag = document.createElement('img');
-  countryFlag.classList.add('js-country-list__flag');
+  countryFlag.classList.add('country-list__flag');
   countryFlag.setAttribute('src', data.flags.svg);
 
   countryList.appendChild(newListElement);
@@ -36,10 +36,10 @@ function showCountryData(obj, targetPlace) {
   //create details of country for every key -> obj keys
   for (const key of keys) {
     const insertHeaderData = document.createElement('p');
-    insertHeaderData.classList.add('js-country-list__heading');
+    insertHeaderData.classList.add('country-list__heading');
 
     const insertHeaderDetails = document.createElement('span');
-    insertHeaderDetails.classList.add('js-country-list__details');
+    insertHeaderDetails.classList.add('country-list__details');
 
     targetPlace.appendChild(insertHeaderData);
 
@@ -81,7 +81,7 @@ function checkMatches(data) {
 //if number of hist is >1 and <11 then create country data details -> invoked by click'ing an country description or arrow
 function showUnfold(el) {
   //el as HTML created element
-  el.classList.add('js-country-list__item--unfold');
+  el.classList.add('country-list__item--unfold');
   // const detailsElements = Array.from(el.children);
 }
 
@@ -119,27 +119,27 @@ document.body.addEventListener('click', e => {
   const iconElementIndex = ListChildrenArr.findIndex(e => e.tagName === 'I');
   const childrenToRemove = ListChildrenArr.filter(e => e.tagName === 'P');
 
-  if (!e.target.classList.contains('js-country-list__item')) {
+  if (!e.target.classList.contains('country-list__item')) {
     return;
   }
 
   if (
-    !e.target.classList.contains('js-country-list__item--unfold') &&
+    !e.target.classList.contains('country-list__item--unfold') &&
     countryList.childElementCount > 1
   ) {
     fetchCountries(e.target.textContent) //getting data from promise
       .then(data => {
         showCountryData(data[0], e.target);
 
-        e.target.classList.add('js-country-list__item--unfold');
+        e.target.classList.add('country-list__item--unfold');
       });
 
     ListChildrenArr[iconElementIndex].classList.add('fa-angle-up');
     ListChildrenArr[iconElementIndex].classList.remove('fa-angle-down');
   }
 
-  if (e.target.classList.contains('js-country-list__item--unfold')) {
-    e.target.classList.toggle('js-country-list__item--unfold');
+  if (e.target.classList.contains('country-list__item--unfold')) {
+    e.target.classList.toggle('country-list__item--unfold');
 
     ListChildrenArr[iconElementIndex].classList.add('fa-angle-down');
     ListChildrenArr[iconElementIndex].classList.remove('fa-angle-up');
